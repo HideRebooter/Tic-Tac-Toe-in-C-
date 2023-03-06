c5f2c495c343e47d70e4d6a77ce6cf793a4575fb
HideRebooter <prajwoladhikari9@gmail.com>
2023-03-06 17:56:02 +0545

    FIrst time adding Git

---
+++ calculator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int num1,num2,opd;
+int main(){
+	printf("1. Press 1 to perform addition\n2. Press 2 to perform subtraction\n3. Press 3 to perform multiplication\n4. Press 4 to perform division");
+	printf("Enter the first number\n");
+	scanf("%d", &num1);
+	printf("Enter your choice 1 or 2 or 3 or 4");
+	scanf("%d", &opd);
+	printf("Enter the second number");
+	scanf("%d", &num2);
+	switch (opd)
+	{
+	case 1:
+	int sum;
+	sum = num+num2;
+	printf("The sum is %d", sum);
+	break;
+	case
+	return 0;
+	}
+}
